903bf990d671344b87a89929a979cfefbc653d78
Joaquin Perez <joaquinperez1988@gmail.com>
2023-02-22 21:51:08 -0300

    java continuacio

--- clase07-javascript/index.js
+++ clase07-javascript/index.js
@@ -120,8 +120,117 @@ console.log("El iva del producto es: ", resultado);
 console.log(`El IVA más el costo del producto es en total: ${totalPago}`);
 
 console.log('================================================');
+console.log('================================================');
+
+/* operadores relacionales */
+/* > mayor que , < menor que , == igual que, >= mayor o igual que, <= menor o igual que
+/* condicionales  */  
+
+edad = 20
+if (true) {
+//si la condicion es verdadera se ejecuta el codigo dentro de las llaves
+console.log("Hola soy el true del condicional if")
 
 
+}
+
+if(false){
+    console.log("nunca voy a imprimir esto")
+    //si es flaso no se ejecuta el codigo de la llave
+}
+
+if(edad > 18){
+    console.log('podes ingresas a la fiesta y comprar bebidas');
+}
+
+if(edad < 18){console.log('no podes entrar, a tu casa');}
+
+if(edad == 21 )//false
+{
+    console.log('podes viajar al exterior sin autorizacion');
+}
+if (edad >= 21) {
+    console.log('podes viajar al exterior sin autorizacion ');
+}else{/* si no es true se ejecuta esto */
+console.log('sos menor de 21 anios necesitas un permiso de tus padres');
+}
 
 
+console.log('================================================');
+console.log('================================================');
 
+let saldo = 3501
+let compra = 3500
+let saldototal = saldo - compra;
+
+
+if (saldo > compra){console.log('compraste los productos');
+console.log(`tu saldo ahora es ${saldototal}`);}else{
+    console.log('compra rechazada');}
+console.log('================================================');
+console.log('================================================');
+
+/* operadores logicos */
+// && y logico - || o logico
+
+let user = 'Joaquin'
+let password = '12345'
+
+if (user == 'Joaquin' && password == '12345'){
+    console.log('Bienvenido al sistema');
+}else{console.log('usuario incorrecto' );}
+
+console.log('================================================');
+console.log('================================================');
+
+if(user == 'Joaquin' || password == '12345'){
+    console.log('Bienvenido al banco');
+}else{
+    if(user |= 'Joaquin'){console.log('usuario incorrecto');}
+//if anidados (probar cambiando pass y usuario)
+    if (password |= '12345'){console.log('pass incorrecta');}
+
+    console.log('raja de aca');
+}
+
+console.log('================================================');
+console.log('================================================');
+
+//Operadores de incremento y decremento == contadores
+//Carrito de compras
+
+let productos = 0
+compras = true
+
+
+if(compras){
+    productos ++;
+    console.log(`Tu carrito tiene ${productos} comprados`);
+
+}else{
+    producto --;
+    console.log(`Tu carrito tiene ${producto} comprados`);
+}
+
+// COntenedor
+
+let totalSuper = 0; 
+
+
+let lacteos = 3000;
+
+totalSuper += lacteos
+
+console.log(`El total a pagar es de ${totalSuper} pesos`);
+
+
+let carnes = 2000;
+
+totalSuper += carnes
+console.log(`El total a pagar es de ${totalSuper} pesos`);
+
+
+let farmacia = 1500;
+
+totalSuper += farmacia
+console.log(`El total a pagar es de ${totalSuper} pesos`);
